06d0994d8f48fff5d593bb9def05dcff4ccad595
IT <it@Is-iMac.local>
2016-12-06 10:22:16 -0300

    abb_obtener_items

--- abb/abb.c
+++ abb/abb.c
@@ -312,7 +312,7 @@ bool _iterar_post_order(nodo_abb_t* nodo, bool visitar(const char *, void *, voi
 	if (!_iterar_post_order(nodo->izq,visitar, extra)) return false;
 	if (!_iterar_post_order(nodo->der, visitar, extra)) return false;
 	if(!visitar(nodo->clave, nodo->valor, extra)) return false;
-	return false;
+	return true;
 }
 
 void abb_post_order(abb_t *arbol, bool visitar(const char *, void *, void *), void *extra) {
@@ -323,7 +323,7 @@ bool almacenar(const char *clave, void *valor, void *items) {
 	abb_item_t** item = items;
 	(*item)->clave = clave;
 	(*item)->valor = valor;
-	(*((abb_item_t**)items))++;
+	(*item)++;
 	return true;
 }
 
